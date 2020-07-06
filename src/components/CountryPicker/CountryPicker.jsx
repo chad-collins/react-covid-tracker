a77345be75f62fb090c1b004a872b816457dad16
Chad Collins <chadcollins.co@gmail.com>
2020-07-06 19:50:31 -0400

    Created initial project structure

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CountryPicker extends Component {
+    render() {
+        return (
+            <div>
+                Country Picker
+            </div>
+        )
+    }
+}
+
+export default CountryPicker

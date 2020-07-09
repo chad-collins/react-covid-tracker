b0a75b87f38270172784b39f4cb270af16975e2e
Chad Collins <chadcollins.co@gmail.com>
2020-07-09 04:08:02 -0400

    updated cards font spacing and size

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,38 +12,38 @@ const Cards = ({ data: { recovered, confirmed, deaths, lastUpdate } }) => {
   return (
     <ul className={styles.list}>
       <li className={cx(styles.card, styles.infected)}>
-        <h2>Infected</h2>
-        <h5>
+        <h2 className={styles.title}>Infected</h2>
+        <h3>
           <CountUp
             start={0}
             end={confirmed.value}
             duration={2.5}
             separator=","
           />
-        </h5>
-        <small>{new Date(lastUpdate).toDateString()}</small>
-        <p>body</p>
+        </h3>
+        <p>Total number of infected</p>
+        <small>Last Update: {new Date(lastUpdate).toDateString()}</small>
       </li>
       <li className={cx(styles.card, styles.recovered)}>
-        <h2>Recovered</h2>
-        <h5>
+        <h2 className={styles.title}>Recovered</h2>
+        <h3>
           <CountUp
             start={0}
             end={recovered.value}
             duration={2.5}
             separator=","
           />
-        </h5>
-        <small>{new Date(lastUpdate).toDateString()}</small>
-        <p>Number of Recoveries</p>
+        </h3>
+        <p>Total number of recoveries</p>
+        <small>Last Update:  {new Date(lastUpdate).toDateString()}</small>
       </li>
       <li className={cx(styles.card, styles.deaths)}>
-        <h2>Deaths</h2>
-        <h5>
+        <h2 className={styles.title}>Deaths</h2>
+        <h3>
           <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
-        </h5>
-        <small>{new Date(lastUpdate).toDateString()}</small>
-        <p>Number of deaths</p>
+        </h3>
+        <p>Total number of deaths</p>
+        <small>Last Update:  {new Date(lastUpdate).toDateString()}</small>
       </li>
     </ul>
   );
